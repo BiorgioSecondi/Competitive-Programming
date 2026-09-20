dd30e5c8ce7e304f87c930b3d5acd1a9d15267bc
Filippo Gargiulo <filippogargiulo.privato@gmail.com>
2026-09-20 18:21:11 +0200

    Removed folder "gara"

--- gara/garaaaaa/p2/bambu.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-
-vector<long long> taglia(int N, int M, int Q, vector<vector<long long>> A, vector<long long> K) {
-    vector<long long> R(Q, 42);
-    for (int i = 0; i < Q; i++) R[i] = 42;
-    return R;
-}
